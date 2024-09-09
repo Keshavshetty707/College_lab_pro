f3951740050d74b6878dbc347c28594244e6ab84
Keshavshetty707 <mkeshav9241217275@gmail.com>
2024-09-09 08:39:56 +0530

    area of circle program

---
+++ in_Python/area-circum.py
@@ -0,0 +1,5 @@
+radius = int(input("enter a radius of a circle:"))
+area = 3.14*radius*radius
+circumference = 2*3.14*radius
+print("area of a given radius is ",area)
+print("circumference of a given radius is:",circumference)
