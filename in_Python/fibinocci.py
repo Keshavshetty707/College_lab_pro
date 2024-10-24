1b96979494304d31264fa4d71d52cfd4d36b6cf9
Keshavshetty707 <mkeshav9241217275@gmail.com>
2024-10-25 04:33:34 +0530

    Fixed Fibonacci series program.

--- in_Python/fibinocci.py
+++ in_Python/fibinocci.py
@@ -1,9 +1,18 @@
-numb = int(input("Enter A number"))
-j = 0
-i = 1
-print(j)
-print(i)
-for _ in range(2, numb):
+numb = int(input("Enter the number of terms: "))
+
+# Initialize the first two terms
+i, j = 0, 1
+
+# Check for valid input
+if numb <= 0:
+ print("Invalid number of terms")
+else :
+ # Print the first term
+ print(0)
+
+if numb >= 1:
+ # Generate the Fibonacci sequence
+ for k in range(2, numb+1 ):
   Ans = i + j
   j = i
   i = Ans
